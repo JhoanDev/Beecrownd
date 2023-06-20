79cf0382540e9b8dad86a4d381c12d97fef13099
JhoanDev <jhoandev.log@gmail.com>
2023-06-20 10:17:05 -0300

    feat: beecrownd 1150

--- UltrapassandoZ.py
+++ UltrapassandoZ.py
@@ -1,12 +1,15 @@
 x = int(input())
 i = 1
 total = x
+
 while True:
     z = int(input())
-    if z > x:
+    if z >= x:
         break
+
 while total < z:
     x += 1
     total += x
     i += 1
+    
 print(i)
