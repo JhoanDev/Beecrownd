61ab42d7eeb8c76970fecc5639613ae36e4afabe
JhoanDev <jhoandev.log@gmail.com>
2024-01-01 15:43:08 -0300

    feat: 1566

---
+++ C/EstruturasEBibliotecas/1566.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int testes, i, j;
+    scanf("%d", &testes);
+    for (i = 0; i < testes; i++)
+    {
+        int *alturas = (int *)calloc(212, sizeof(int));
+        int pessoas;
+        int temp;
+        int bolean = 1;
+        scanf("%d", &pessoas);
+        for (j = 0; j < pessoas; j++)
+        {
+            scanf("%d", &temp);
+            alturas[temp - 20]++;
+        }
+        for (j = 0; j <= 210; j++)
+        {
+            if (alturas[j] > 0)
+            {
+                while (alturas[j] != 0)
+                {
+                    if (bolean)
+                    {
+                        printf("%d", j + 20);
+                        bolean = 0;
+                    }
+                    else
+                    {
+                        printf(" %d", j + 20);
+                    }
+                    alturas[j]--;
+                }
+            }
+        }
+        printf("\n");
+        free(alturas);
+    }
+
+    return 0;
+}
