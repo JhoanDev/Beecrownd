812c10020787a2eafbb16ab925bd880ba3c5acd7
JhoanDev <jhoandev.log@gmail.com>
2025-04-21 09:32:13 -0300

    feat: 2416

---
+++ Bee/2_Ad-Hoc/2416.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _                         \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
+#define endl '\n'
+
+int main()
+{
+    _ 
+    int c, n;
+    cin >> c >> n;
+    cout << c%n << endl;
+    return 0;
+}
