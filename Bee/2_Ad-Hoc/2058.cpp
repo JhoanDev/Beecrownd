99c2d801ddb42f1c56d7f21bacb3e6138e7906b0
JhoanDev <jhoandev.log@gmail.com>
2025-04-21 09:38:37 -0300

    feat: 2058

---
+++ Bee/2_Ad-Hoc/2058.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _                         \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);
+#define endl '\n'
+
+int main()
+{
+    _ 
+    int  n;
+    cin >> n;
+    cout << n-2 << endl;
+    return 0;
+}
